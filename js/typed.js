792a8d838fedd5a6f3701842c1da228f5e23603c
anas <anaseswi21+1@gmail.com>
2024-11-05 07:43:53 +0200

    Add files via upload

---
+++ js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'pro audiomaker','500k networth','"define rconsole"','fuck skids','you just got beamed','founder of Apple LOLLLLLLLLLL','https://realrtq.github.io/RTQ/ :D'],
+  typeSpeed : 50,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
